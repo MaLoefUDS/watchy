dc50e4f23411309815376cd212c2745d188d7cee
Maximilian Löffler <s8maloef@stud.uni-saarland.de>
2024-10-10 02:19:58 +0200

    Add basic CSS

--- static/load_videos.js
+++ static/load_videos.js
@@ -17,10 +17,8 @@ const add_video = function(video) {
     const feed = document.getElementById('feed');
     const video_element = document.createElement('div');
     video_element.innerHTML = `
-        <div class="video">
-            <iframe width="420" height="315"
-              src="https://www.youtube.com/embed/${video}?mute=1">
-            </iframe>
+        <div id="video-container">
+            <iframe id="video" src="https://www.youtube.com/embed/${video}?mute=1"></iframe>
         </div>
     `;
     feed.appendChild(video_element);
